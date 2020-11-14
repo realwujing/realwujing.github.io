24841309025b73c47529803d1b3a87a6ede268b2
wujing <178955347@qq.com>
2020-11-15 01:49:33 +0800

    extern class

---
+++ test/Test1.h
@@ -0,0 +1,12 @@
+//
+// Created by wujing on 2020/11/15.
+//
+
+#ifndef C__PROJECT_TEST1_H
+#define C__PROJECT_TEST1_H
+
+
+extern char g_str[]; // 声明全局变量g_str
+void fun1();
+
+#endif //C__PROJECT_TEST1_H
---
+++ test/Test3.cpp
@@ -0,0 +1,12 @@
+//
+// Created by wujing on 2020/11/15.
+//
+
+#include "Test3.h"
+#include <iostream>
+
+void Test3::print() {
+    std::cout << "nihao" << std::endl;
+}
+
+Test3 test3;
