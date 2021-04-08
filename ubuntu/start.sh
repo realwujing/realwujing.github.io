2735675b7b62a378826c268bef544ddee3ac2b52
RealWuJing <178955347@qq.com>
2021-04-08 22:04:59 +0800

    start.sh

--- ubuntu/start.sh
+++ ubuntu/start.sh
@@ -7,6 +7,7 @@ echo "2988" | sudo -S service mysql start
 echo '2. starting ssh...'
 echo "2988" | sudo -S service ssh start
 
-echo "start success..." >&1 | tee -a /usr/local/start.log
+# 开机启动服务成功
+echo $(date "+%Y-%m-%d %H:%M:%S") start success... >&1 | tee -a /usr/local/start.log
 
 tail -f /dev/null
