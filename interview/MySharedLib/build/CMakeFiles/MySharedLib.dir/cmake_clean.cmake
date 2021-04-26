3d82061d3342d32b83058a501aa8a8565f0de817
RealWuJing <178955347@qq.com>
2021-04-26 09:41:40 +0800

    lib

---
+++ interview/MySharedLib/build/CMakeFiles/MySharedLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySharedLib.dir/library.cpp.o"
+  "libMySharedLib.pdb"
+  "libMySharedLib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MySharedLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
