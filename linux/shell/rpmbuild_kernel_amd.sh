4eca59b6d47b9dac3199519536db6b212567bd07
wujing <realwujing@qq.com>
2025-03-18 17:14:22 +0800

    linux: kernel: rpmbuild_kernel_amd.sh
    Signed-off-by: wujing <realwujing@qq.com>
    Signed-off-by: QiLiang Yuan <yuanql9@chinatelecom.cn>

--- linux/shell/rpmbuild_kernel_amd.sh
+++ linux/shell/rpmbuild_kernel_amd.sh
@@ -80,4 +80,8 @@ sed -i -E '0,/%define with_kabichk 1/! {/%define with_kabichk 1/s/1/0/}' build/k
 # 在版本号后面追加.$(git rev-parse HEAD | head -c 12)，$(git rev-parse HEAD | head -c 12)指向最后一次提交的commit hash的前12位
 sed -i "/%global pkg_release/s/$/\.$(git rev-parse HEAD | head -c 12)/" build/kernel.spec
 
-rpmbuild -ba build/kernel.spec
+rm -rf /root/rpmbuild/SOURCES/kernel.tar.gz
+tar --xform="s/^./kernel/" --exclude=".git" -chzf /root/rpmbuild/SOURCES/kernel.tar.gz .
+cp build/* /root/rpmbuild/SOURCES
+
+time rpmbuild -ba build/kernel.spec
