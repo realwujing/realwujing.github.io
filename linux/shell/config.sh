b32a6096631f74c2200f6c32da451ad974ef32b4
wujing <realwujing@qq.com>
2025-06-20 09:16:10 +0800

    linux/shell/config.sh: easy config vim and git
    Signed-off-by: wujing <realwujing@qq.com>
    Signed-off-by: QiLiang Yuan <yuanql9@chinatelecom.cn>

--- linux/shell/config.sh
+++ linux/shell/config.sh
@@ -77,4 +77,9 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s /home/wujing/code/realwujing.github.io/linux/vim/.vimrc .vimrc
 vim +PluginInstall +qall
 
-tar -zcvf ~/vim-git.tar.gz ~/code/realwujing.github.io/git/.gitcommit_template ~/code/realwujing.github.io/git/.gitconfig ~/code/realwujing.github.io/git/.gitignore_global ~/.vim ~/.vimrc
+tar -zcvf ~/vim-git.tar.gz \
+    -C ~/code/realwujing.github.io/git .gitcommit_template \
+    -C ~/code/realwujing.github.io/git .gitconfig \
+    -C ~/code/realwujing.github.io/git .gitignore_global \
+    -C ~/code/realwujing.github.io/linux/vim .vimrc \
+    -C ~ .vim
